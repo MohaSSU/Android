1615e66153631ece7ebab87d5053a5a754680f74
rudeore-098 <rudeore0928@gmail.com>
2024-11-24 20:35:15 +0900

    [FEAT/24] : REAL TIME Login logic

--- app/src/main/java/com/example/mohassu/TimeTableActivity.java
+++ app/src/main/java/com/example/mohassu/TimeTableActivity.java
@@ -14,7 +14,7 @@ public class TimeTableActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
         // Initialize TimetableView
         timetableView = findViewById(R.id.timetable);
