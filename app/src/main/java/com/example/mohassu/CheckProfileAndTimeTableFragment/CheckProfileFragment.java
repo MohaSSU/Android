e9fc353eda6fa3f2f0fbe74198a5ff6820925a86
2ghrms <aa020228@gmail.com>
2024-12-03 11:34:45 +0900

    :sparkles:[FEAT/#37] : 시간표 input/edit 프래그먼트 생성

--- app/src/main/java/com/example/mohassu/CheckProfileAndTimeTableFragment/CheckProfileFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.mohassu.CheckProfileAndTimeTableFragment;
-
-import androidx.fragment.app.Fragment;
-
-public class CheckProfileFragment extends Fragment {
-}
---
+++ app/src/main/java/com/example/mohassu/DialogFragment/ClassInputDialogFragment.java
@@ -0,0 +1,4 @@
+package com.example.mohassu.DialogFragment;
+
+public class ClassInputDialogFragment {
+}
